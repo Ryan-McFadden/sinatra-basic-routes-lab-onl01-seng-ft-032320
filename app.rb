c8eed811079733fbf58cbd9985f8ba9ad37843f0
Ryan-McFadden <ryanmcfadden123@gmail.com>
2020-05-01 19:58:07 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Ryan McFadden"
+    end
+
+    get '/hometown' do
+        "My hometown is Safety Harbor, FL"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Jim's Theme from Treasure Planet"
+    end
 end
